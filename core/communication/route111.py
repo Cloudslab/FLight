9102c23f4f7e02480eb0b2ffed85546a3fea65ec
Wuji Zhu <wujizhu1999@outlook.com>
2022-06-10 14:34:50 +1000

    message test

--- core/communication/route111.py
+++ core/communication/route111.py
@@ -1,7 +1,25 @@
 from utils.component.communicator import Communicator
 from utils.component.communicator import ComponentRole
 from utils.types.basic.address import Address
+from utils.connection.message.received import MessageReceived
 from typing import Tuple
+
+class FlBasicCommunicator(Communicator):
+
+    def __int__(self,
+                role: ComponentRole,
+                addr: Address,
+                portRange: Tuple[int, int],
+                logLevel: int,
+                masterAddr: Address,
+                remoteLoggerAddr: Address,
+                ignoreSocketError: bool = False):
+
+        super.__init__(role, addr, portRange, logLevel, masterAddr, remoteLoggerAddr, ignoreSocketError)
+
+    def handlerMessage(self, message: MessageReceived):
+        print(message.toDict())
+
 
 if __name__ == "__main__":
 
@@ -13,6 +31,11 @@ if __name__ == "__main__":
     addr_holder = ["0.0.0.0", 5000]
     master_addr = remote_logger_addr = addr_holder
 
-    c = Communicator(componentRole, master_addr, port_range, log_lvl, addr_holder, remote_logger_addr)
+    c = FlBasicCommunicator(componentRole, master_addr, port_range, log_lvl, addr_holder, remote_logger_addr)
+
+
+
 
     print("HW")
+
+
