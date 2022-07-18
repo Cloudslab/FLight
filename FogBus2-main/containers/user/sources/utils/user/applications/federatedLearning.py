59434531131a9ecd18673988783c0e07b6908934
wujizhu1999 <wujizhu1999@outlook.com>
2022-07-18 20:58:54 +1000

    user

---
+++ FogBus2-main/containers/user/sources/utils/user/applications/federatedLearning.py
@@ -0,0 +1,48 @@
+from time import time
+from pprint import pformat
+from .base import ApplicationUserSide
+from ...component.basic import BasicComponent
+
+
+class FederatedLearning(ApplicationUserSide):
+
+    def __init__(
+            self,
+            videoPath: str,
+            targetHeight: int,
+            showWindow: bool,
+            basicComponent: BasicComponent):
+        super().__init__(
+            appName='FederatedLearning',
+            videoPath=videoPath,
+            targetHeight=targetHeight,
+            showWindow=showWindow,
+            basicComponent=basicComponent)
+
+    def prepare(self):
+        pass
+
+    def _run(self):
+        self.basicComponent.debugLogger.info(
+            'Application is running: %s', self.appName)
+
+        inputData = {
+        }
+
+        # put it in to data uploading queue
+        self.dataToSubmit.put(inputData)
+        lastDataSentTime = time()
+
+        # wait for all the 4 results
+        while True:
+            result = self.resultForActuator.get()
+
+            responseTime = (time() - lastDataSentTime) * 1000
+            self.responseTime.update(responseTime)
+            self.responseTimeCount += 1
+
+            if 'taskID0' in result and 'taskID1' in result and 'taskID2' in result:
+                break
+
+        self.basicComponent.debugLogger.info(
+            'Received all the 3 ID: \r\n%s', pformat(result))
