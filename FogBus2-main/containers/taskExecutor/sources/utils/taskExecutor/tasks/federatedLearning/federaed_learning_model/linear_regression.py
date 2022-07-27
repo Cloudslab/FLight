ec8a5a292baf5d54ba1542353e704f65d6cdc03b
wujizhu1999 <wujizhu1999@outlook.com>
2022-07-28 02:31:15 +1000

    step finish

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedLearning/federaed_learning_model/linear_regression.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedLearning/federaed_learning_model/linear_regression.py
@@ -18,7 +18,63 @@ class linear_regression(base_model):
         self.uuid = uuid
 
     def step(self, train_data):
-        #for large ds, train_data can be necessary information to load dataset
+        # for large ds, train_data can be necessary information to load dataset
+        x, y = train_data
+
+        def grad(x_ele, y_ele):
+            l = self.w * x_ele + self.b - y_ele
+            return l, l * x_ele
+
+        def grad_all(x_list, y_list):
+            if len(x_list) != len(y_list) or len(x_list) == 0:
+                # should be an error here
+                return None
+
+            grad_B, grad_W = 0, 0
+            for x_ele, y_ele in zip(x_list, y_list):
+                grad_b, grad_w = grad(x_ele, y_ele)
+                grad_B += grad_b
+                grad_W += grad_w
+
+            return grad_B / len(x_list), grad_W / len(x_list)
+
+        grad_B, grad_W = grad_all(x, y)
+
+        self.b = self.b - self.lr * grad_B
+        self.w = self.w - self.lr * grad_W
+
+    def ack_ready(self, role, ptr):
+        pass
+    def add_peer(self, ptr):
+        pass
+    def add_client(self, ptr):
+        pass
+    def add_server(self, ptr):
+        pass
+    def ask_next(self, role, ptr):
+        pass
+    def can_federate(self):
+        return True
+    def can_fetch(self, role, ptr):
+        return True
+    def can_load(self, role, ptr):
+        return True
+    def export(self):
+        pass
+    def federate(self, fl_algo):
+        pass
+    def fetch_client(self, client_id):
+        pass
+    def fetch_peer(self, peer_id):
+        pass
+    def fetch_server(self, server_id):
+        pass
+    def load(self, data):
+        pass
+if __name__ == "__main__":
+    model = linear_regression(0,0,0.01,0)
+    for i in range(1000):
+        model.step(([1,2,3,4,5],[3,5,7,9,11]))
 
 
-
+    print(model.w, model.b)
