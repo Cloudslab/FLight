24554d69143da91d4541a1ec55f44e667b754609
wujizhu1999 <wujizhu1999@outlook.com>
2022-07-29 23:22:03 +1000

    tiny change

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedServer.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedServer.py
@@ -4,11 +4,11 @@ class FederatedServer(BaseTask):
     def __init__(self):
         super().__init__(taskID=221, taskName='FederatedServer')
         self.worker_addr = []
-        self.server_addr
+        self.server_addr = None
     def exec(self, inputData):
         self.server_addr = inputData["self_addr"]
         self.worker_addr.append(inputData["child_addr"])
 
-        if len(self.worker_addr) >= 2:
+        if len(self.worker_addr) >= 3:
             inputData["Ress"] = {"server_addr":self.server_addr, "worker_addr":self.worker_addr}
             return inputData
