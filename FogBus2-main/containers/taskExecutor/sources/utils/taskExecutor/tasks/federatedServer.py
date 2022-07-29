da63939676ca2cc6f8f2cc732b052503da455755
wujizhu1999 <wujizhu1999@outlook.com>
2022-07-30 03:01:25 +1000

    idk

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedServer.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedServer.py
@@ -42,9 +42,8 @@ class FederatedServer(BaseTask):
         lr = linear_regression(0, 0, 0.01)
         for (addr, port) in self.worker_addr:
             lr.add_client((addr, 54321))
-        time.sleep(3)
-        #while len(lr.client) < 3 and lr.ready_to_train_client < 3:
-        #    time.sleep(0.01)
+        while len(lr.client) < 3 and lr.ready_to_train_client < 3:
+            time.sleep(0.01)
         #
         #
         #data_warehouse.get(lr.client[0][0]).load_data = one_x
