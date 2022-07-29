f8aab6766f83be93ad0a9d3d89510419e4521f78
wujizhu1999 <wujizhu1999@outlook.com>
2022-07-30 00:39:19 +1000

    should work

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedLearning1.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedLearning1.py
@@ -1,10 +1,10 @@
 from .base import BaseTask
-#from federatedLearning.communicate.router import router_factory
+from federatedLearning.communicate.router import router_factory
 
 class federatedLearning1(BaseTask):
     def __init__(self):
         super().__init__(taskID=232, taskName='FederatedLearning1')
 
     def exec(self, inputData):
-        #router_factory.get_router((inputData["self_addr"],54321))
+        router_factory.get_router((inputData["self_addr"][0],54321))
         pass
