8da8d830f4407415c1d5a769dc197a1595320678
wujizhu1999 <wujizhu1999@outlook.com>
2022-07-29 20:56:49 +1000

    local work

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedLearning/trial.py
+++
@@ -1,6 +0,0 @@
-from communicate.router import router
-from communicate.router import router_factory
-from federaed_learning_model.linear_regression import linear_regression
-
-if __name__ == "__main__":
-    router_factory.get_router(("127.0.0.1", 12345))
