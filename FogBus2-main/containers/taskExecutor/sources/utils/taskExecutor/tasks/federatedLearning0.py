76366286933f9af88d89ffa284a86b3fc26886c5
wujizhu1999 <wujizhu1999@outlook.com>
2022-07-30 00:14:23 +1000

    router in

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedLearning0.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedLearning0.py
@@ -1,8 +1,9 @@
 from .base import BaseTask
+from federatedLearning.communicate.router import router_factory
 
 class federatedLearning0(BaseTask):
     def __init__(self):
         super().__init__(taskID=231, taskName='FederatedLearning0')
 
     def exec(self, inputData):
-        pass
+        router_factory.get_router((inputData["self_addr"],12345))
--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedLearning1.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedLearning1.py
@@ -1,8 +1,9 @@
 from .base import BaseTask
+from federatedLearning.communicate.router import router_factory
 
 class federatedLearning1(BaseTask):
     def __init__(self):
         super().__init__(taskID=232, taskName='FederatedLearning1')
 
     def exec(self, inputData):
-        pass
+        router_factory.get_router((inputData["self_addr"],12345))
