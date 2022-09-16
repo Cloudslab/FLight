bcc8dbcfad528b95c411bcf01303a36b63ac3fde
wujizhu1999 <wujizhu1999@outlook.com>
2022-09-17 02:47:32 +1000

    ftp

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedServer.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedServer.py
@@ -53,11 +53,11 @@ class FederatedServer(BaseTask):
             time.sleep(WAITING_TIME_SLOT)
 
         for cli in model.get_client():
-            model.fetch_client(cli)
+            model.step_remote(cli)
         for ser in model.get_server():
-            model.fetch_server(ser)
+            model.step_remote(ser)
         while (len(model.get_remote_fetch_model_credential("s")) + len(
-                model.get_remote_fetch_model_credential("c"))) < 3:
+                model.get_remote_fetch_model_credential("c"))) < 2:
             time.sleep(0.01)
         for cre in model.get_remote_fetch_model_credential("s"):
             model.download_model(cre, "s")
