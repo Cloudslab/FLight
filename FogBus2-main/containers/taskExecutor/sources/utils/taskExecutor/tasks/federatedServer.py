c9444d9399e568129044586c61a8de6fd5455621
wujizhu1999 <wujizhu1999@outlook.com>
2022-10-20 23:36:23 +1100

    bring cpu in

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedServer.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/federatedServer.py
@@ -110,11 +110,11 @@ class FederatedServer(BaseTask):
                 inputData["debug_logger"].info("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
                 if model.model.accuracy > 80:
                     tr1 = time.time() - t
-                    inputData["debug_logger"].info(str(model.model.accuracy.data) + " achieved at time: " + str(tr1))
+                    inputData["debug_logger"].info(str(model.model.accuracy.item()) + " achieved at time: " + str(tr1))
                     break
                 else:
                     tr1 = time.time() - t
-                    inputData["debug_logger"].info(str(model.model.accuracy.data) + " achieved at time: " + str(tr1))
+                    inputData["debug_logger"].info(str(model.model.accuracy.item()) + " achieved at time: " + str(tr1))
 
 
 
