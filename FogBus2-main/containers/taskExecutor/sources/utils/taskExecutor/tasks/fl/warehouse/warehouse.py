a05caa7bbfab8f9bc20ca2acdf8110c73dde5075
wujizhu1999 <wujizhu1999@outlook.com>
2022-11-12 22:41:20 +1100

    link warehouse with data_warehouse & model_warehouse

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/warehouse/warehouse.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/warehouse/warehouse.py
@@ -10,8 +10,8 @@ from .model_warehouse import model_warehouse
 
 class warehouse:
     def __init__(self):
-        self.data_warehouse = data_warehouse()
-        self.model_warehouse = model_warehouse()
+        self._data_warehouse = data_warehouse()
+        self._model_warehouse = model_warehouse()
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "instance"):
@@ -19,13 +19,17 @@ class warehouse:
         return getattr(cls, "instance")
 
     def set_model(self, model, model_id: str = None, storage: str = None):
-        pass
+        if not model_id: model_id = uuid.uuid1()
+        actual_model_id = self._model_warehouse.set_model(model, model_id, storage)
+        return actual_model_id
 
     def get_model(self, model_id: str):
-        pass
+        return self._model_warehouse.get_model(model_id)
 
     def set_data(self, data, data_id: str = None, storage: str = None):
-        pass
+        if not data_id: data_id = uuid.uuid1()
+        actual_data_id = self._data_warehouse.set_data(data, data_id, storage)
+        return actual_data_id
 
     def get_data(self, data_id: str):
-        pass
+        return self._data_warehouse.get_data(data_id)
