e036efe9fe884b90e2700bb6179242b471582bad
wujizhu1999 <wujizhu1999@outlook.com>
2022-11-28 23:41:25 +1100

    ram storage and retrieve

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/warehouse/Accessory/abstract_Accessory.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/warehouse/Accessory/abstract_Accessory.py
@@ -1,16 +1,16 @@
 """ Accessory that every other accessory should follow """
-
+import uuid
 from abc import ABC, abstractmethod
 from uuid import uuid1
 
 class abstract_accessory(ABC):
 
     @abstractmethod
-    def set(self, args: dict):
+    def set(self, args: dict, data_id: uuid.UUID = None):
         pass
 
     @abstractmethod
-    def get(self, args: dict):
+    def get(self, args: dict, data_id: uuid.UUID):
         pass
 
     @staticmethod
--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/warehouse/Accessory/ram_Accessory.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/warehouse/Accessory/ram_Accessory.py
@@ -1,14 +1,21 @@
 """ This class represents data stored on RAM, everything stored in a dictionary"""
 
 from .abstract_Accessory import abstract_accessory
+from collections import defaultdict
+import uuid
 
 
 class ram_accessory(abstract_accessory):
     def __init__(self):
-        pass
+        self._storage = defaultdict(None)
 
-    def get(self, args: dict):
-        pass
+    def get(self, args: dict, data_id: uuid.UUID):
+        return self._storage.get(uuid)
 
-    def set(self, args: dict):
-        pass
+    def set(self, args: dict, data_id: uuid.UUID = None):
+        if data_id:
+            data_id = self.get_new_id()
+        if "data" in args:
+            self._storage[data_id] = args["data"]
+        if "model" in args:
+            self._storage[data_id] = args["model"]
