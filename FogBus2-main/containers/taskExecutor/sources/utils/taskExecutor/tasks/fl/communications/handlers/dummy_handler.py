3a671583bb7ab09f2148244d98c2faca0a979439
wujizhu1999 <wujizhu1999@outlook.com>
2022-12-02 19:13:57 +1100

    dummy handler

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/communications/handlers/dummy_handler.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/communications/handlers/dummy_handler.py
@@ -7,6 +7,7 @@ class dummy_handler(abstract_handler):
     def __call__(self, conn, reply_addr, *args, **kwargs):
         sub_event = (conn.recv(self.EVENT_STRING_LEN)).decode('utf-8')
         data_received = pickle.loads((conn.recv(2048)))
+        print("Received incoming event:\n")
         print(sub_event, data_received)
 
     def __init__(self):
