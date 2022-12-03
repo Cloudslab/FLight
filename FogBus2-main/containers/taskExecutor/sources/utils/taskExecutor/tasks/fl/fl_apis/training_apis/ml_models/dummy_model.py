eb30bbd6e288ca7ab6a5cbdd0ecfa817cade50a8
wujizhu1999 <wujizhu1999@outlook.com>
2022-12-04 08:31:49 +1100

    a bit lock implementation

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/fl_apis/training_apis/ml_models/dummy_model.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/fl_apis/training_apis/ml_models/dummy_model.py
@@ -1,11 +1,12 @@
 """Sample Implementation for test purpose"""
 
 from .ml_model import ml_model
-
+from threading import Lock
 
 class dummy_model(ml_model):
     def __init__(self, additional_args=None):
         super(dummy_model, self).__init__(additional_args)
+        self._model_lock = Lock()
         self.content = ""
         self.count = 0
 
@@ -19,8 +20,12 @@ class dummy_model(ml_model):
         self.count += 1
 
     def to_dict(self):
-        return {"count": self.count}
+        self._model_lock.acquire()
+        res = {"count": self.count}
+        self._model_lock.release()
+        return res
 
     def from_dict(self, dict_model: dict):
+        self._model_lock.acquire()
         self.count = dict_model["count"]
-
+        self._model_lock.release()
