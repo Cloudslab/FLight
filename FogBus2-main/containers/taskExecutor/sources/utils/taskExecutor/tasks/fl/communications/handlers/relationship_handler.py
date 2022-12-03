c3c06ab3d24afcd650309a32f2da0ca206e3793a
wujizhu1999 <wujizhu1999@outlook.com>
2022-12-03 17:11:03 +1100

    why no response

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/communications/handlers/relationship_handler.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/communications/handlers/relationship_handler.py
@@ -19,35 +19,29 @@ class relationship_handler(abstract_handler):
         data_received = pickle.loads((conn.recv(2048)))
         sub_event = data_received["sub_event"]
         if sub_event == self.sub_events.add_server:
-            x = 1
             model_name, reply_id, additional_args = data_received["model_name"], data_received["reply_uuid"], data_received["additional_args"]
             new_model = self._name_to_model[model_name](additional_args)
             remote_ptr = model_pointer(reply_id, reply_addr)
             new_model.add_ptr("s", remote_ptr, additional_args)
             new_model.ack_add("c", remote_ptr, None)
-            x.sot()
         if sub_event == self.sub_events.add_client:
-            y = 1
             model_name, reply_id, additional_args = data_received["model_name"], data_received["reply_uuid"], data_received["additional_args"]
             new_model = self._name_to_model[model_name](additional_args)
             remote_ptr = model_pointer(reply_id, reply_addr)
             new_model.add_ptr("c", remote_ptr, additional_args)
             new_model.ack_add("s", remote_ptr, None)
-            y.sot()
         if sub_event == self.sub_events.add_peer:
-            z = 1
             model_name, reply_id, additional_args = data_received["model_name"], data_received["reply_uuid"], data_received["additional_args"]
             new_model = self._name_to_model[model_name](additional_args)
             remote_ptr = model_pointer(reply_id, reply_addr)
             new_model.add_ptr("p", remote_ptr, additional_args)
             new_model.ack_add("p", remote_ptr, None)
-            z.sot()
         if sub_event == self.sub_events.ack_add:
-            a = 1
-            a.sot()
             reply_id, additional_args, local_id, role_of_remote = data_received["reply_uuid"], data_received["additional_args"], data_received["remote_uuid"], data_received["role_of_this_model"]
             model = warehouse().get_model(local_id)
             if model:
+                x = 1
+                x.sot()
                 model.add_ptr(role_of_remote, model_pointer(reply_id, reply_addr), additional_args)
 
     def __init__(self):
