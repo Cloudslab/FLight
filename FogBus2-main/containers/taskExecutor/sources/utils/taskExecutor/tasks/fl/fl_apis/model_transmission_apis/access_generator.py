0e1d75d1fa76ab20a3a99ab8d4c44bdabaf9d72c
wujizhu1999 <wujizhu1999@outlook.com>
2022-12-04 17:27:28 +1100

    remove fetcher and include into model communication manager

--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/fl_apis/model_transmission_apis/access_generator.py
+++
@@ -1,6 +0,0 @@
-"""Store the file on ftp server and response credentials so remote nodes can download"""
-
-
-class access_generator:
-    def __init__(self):
-        pass
--- FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/fl_apis/model_transmission_apis/model_transmission_manager.py
+++ FogBus2-main/containers/taskExecutor/sources/utils/taskExecutor/tasks/fl/fl_apis/model_transmission_apis/model_transmission_manager.py
@@ -1,11 +1,12 @@
 """Handle communication of model weights between different parties"""
 from ..training_apis.ml_models.ml_model import ml_model
 from ..relationship_apis.model_pointer import model_pointer
-from .fetcher import fetcher
+from ...communications.router import router
+from ...communications.handlers.model_communication_handlers import model_communication_handler
 
 class model_transmission_manager:
     def __init__(self):
-        self._fetcher = fetcher()
+        pass
 
     def generate_access(self, remote_ptr: model_pointer, model_object: ml_model):
         pass
@@ -16,8 +17,15 @@ class model_transmission_manager:
     def load_model_to_cache(self, cache: dict, cache_type):
         pass
 
-    def fetch_remote(self, self_uuid, remote_ptr: model_pointer, additional_args=None):
-        return self._fetcher.fetch_remote(self_uuid, remote_ptr, additional_args)
+    @staticmethod
+    def fetch_remote(self_uuid, remote_ptr: model_pointer, additional_args=None):
+        r = router.get_default_router()
+        r.send(remote_ptr.address, model_communication_handler.name, {
+            "sub_event": model_communication_handler.sub_event.fetch_model_weights,
+            "reply_uuid": self_uuid,
+            "remote_uuid": remote_ptr.uuid,
+            "additional_args": additional_args
+        })
 
     def download_model(self, local_file_path, credentials):
         pass
